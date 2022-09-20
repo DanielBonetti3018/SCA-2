3817131ba2b07706f7b1e56a8ff55ad8bc5c39f0
danielbonetti3018 <daniel.almeida6@granbery.metodista.br>
2022-09-20 19:07:13 -0300

    commit

---
+++ sca-master/sca-master/aplic/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from .models import Curso, Professor, Aluno
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'carga_horaria')
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'titulacao', 'curso')
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('matricula', 'nome', 'curso')
+# @admin.register(Disciplina)
+# class
